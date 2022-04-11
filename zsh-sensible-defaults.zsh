b42a735980a68f0759cdf219427628d01280bccc
Troels Bjørnskov <troels@bjoernskov.org>
2022-04-11 15:37:43 +0200

    Include zsh colors

--- zsh-sensible-defaults.zsh
+++ zsh-sensible-defaults.zsh
@@ -1,4 +1,141 @@
 # - - - - - - - - - - - - - - - - - - - -
+# - - - - - - - -COLORS - - - - - - - - -
+# - - - - - - - - - - - - - - - - - - - -
+
+
+export LS_COLORS="${LS_COLORS:-*.json=35:*.JSON=35:*.pdf=31:*.PDF=31:*.djvu=31:*.DJVU=31:*.epub=31:*.EPUB=31:*.zip=33:*.ZIP=33:*.xz=33:*.XZ=33:*.gz=38;5;215:*.zst=38;5;215:*.tz=38;5;215:*.sublime-package=33:*.jpg=34:*.JPG=34:*.jpeg=34:*.JPEG=34:*.png=34:*.PNG=34:*.webp=34:*.WEBP=34:*.svg=34:*.SVG=34:*.gif=34:*.GIF=34:*.bmp=34:*.BMP=34:*.tif=34:*.TIF=34:*.tiff=34:*.TIFF=34:*.psd=34:*.PSD=34:*.mkv=35:*.mp4=35:*.mov=35:*.mp3=35:*.avi=35:*.mpg=35:*.m4v=35:*.oga=35:*.MKV=35:*.MP4=35:*.MOV=35:*.MP3=35:*.AVI=35:*.MPG=35:*.M4V=35:*.OGA=35:*.doc=36:*.docx=36:*.odt=36:*.ods=36:*.xlsx=36:*.xls=36:*.DOC=36:*.DOCX=36:*.ODT=36:*.ODS=36:*.XLSX=36:*.XLS=36:*.html=38;5;208:*.HTML=38;5;208:*.log=38;5;18:*.LOG=38;5;18:*.zwc=38;5;18:*.md=33;01:*.MD=33;01:bd=35;1:ex=48;5;153:cd=35;1:di=36:ln=0;36;1:*.sublime-keymap=48;5;154;38;5;208:*.sublime-settings=48;5;154;38;5;208:*.sublime-project=48;5;154;38;5;208:*.sublime-color-scheme=48;5;154;38;5;208:*.sublime-commands=48;5;154;38;5;208:*.sublime-mousemap=48;5;154;38;5;208:*.sublime-syntax=48;5;154;38;5;208:*.sublime-build=48;5;154;38;5;208:*.py=32}"
+
+autoload -U colors && colors
+
+zle_highlight=(region:bg=153 special:bg=153
+               suffix:bg=153 paste:bg=153 isearch:bg=153)
+
+_raw_to_zsh_color() {
+    if [[ $1 =~ "^38;5;.+$" ]]; then
+        print -n "%F{${1##*;}}"
+        return
+    fi
+    if [[ $1 =~ "^48;5;.+$" ]]; then
+        print -n "%B{${PART##*;}}"
+        return
+    fi
+    local -a parts=( ${(s[;])1} )
+    local val=""
+    for part in $parts; do
+        if [[ ${#part} -eq 1 ]]; then
+            val+="%B"
+        else
+            if [[ "${part:0:1}" == "3" ]]; then
+                val+="%F{${part:1:2}}"
+            elif [[ "${part:0:1}" == "4" ]]; then
+                val+="%K{${part:1:2}}"
+            fi
+        fi
+    done
+    print -n $val
+}
+
+() {
+    typeset -agU _ls_colors_array
+    _ls_colors_array=("${(@s/:/)LS_COLORS}")
+
+    local -a a
+    typeset -gA _ls_colors_dict
+    local elem elem_no_asterisk
+    for elem in ${_ls_colors_array:l}; do
+
+        if [[ "${elem:0:2}" != '*.' ]]; then
+            if [[ "${elem:0:2}" == "di" ]]; then
+                _di_color_raw="${elem[4,-1]}"
+            elif [[ "${elem:0:2}" == "ex" ]]; then
+                _ex_color_raw="${elem[4,-1]}"
+            elif [[ "${elem:0:2}" == "bd" ]]; then
+                _bd_color_raw="${elem[4,-1]}"
+            elif [[ "${elem:0:2}" == "ln" ]]; then
+                _ln_color_raw="${elem[4,-1]}"
+            elif [[ "${elem:0:2}" == "cd" ]]; then
+                _cd_color_raw="${elem[4,-1]}"
+            fi
+        else
+            elem_no_asterisk=${elem:2}
+            a=("${(@s/=/)elem_no_asterisk}")
+
+            if [[ -n ${a[1]} ]]; then
+                _ls_colors_dict[${a[1]}]=${a[2]}
+            fi
+        fi
+    done
+}
+
+_colorizer_abs_path() {
+    local this_dir="${${1}/$HOME/\~}"
+    _colorizer $1
+}
+
+_colorizer() {
+    (( ${#@} > 1 )) && print "\x1b[31;1m_colorizer: MORE THAN ONE INPUT ARG\x1b[39m" && return 42
+    local file=${(QQQ)1}
+
+    [[ -z $this_dir ]] && local this_dir="${${${1}/$PWD\//}/$HOME/\~}"
+    local leading_slash=''
+    if [[ "${this_dir:0:1}" == '/' ]] && [[ ${#this_dir} -gt 1 ]]; then
+        leading_slash='/'
+    fi
+
+    local -a dir_parts=( ${(s[/])this_dir} )
+    local tmp_dir=''
+    local _file_and_color
+
+    for part in ${dir_parts[0,-2]}; do
+        tmp_dir+="\033[${_di_color_raw}m${part}\033[39m/"
+    done
+
+    if [[ -f "$file" ]]; then
+        if [[ -x "$file" ]]; then
+            _file_and_color="\x1b[${_ex_color_raw}m${dir_parts[-1]}\x1b[39m"
+        else
+            local _file_color=${_ls_colors_dict[${1:e:l}]}
+            _file_and_color="\x1b[${_file_color:-39}m${dir_parts[-1]}"
+        fi
+    elif [[ -d "$file" ]]; then
+        _file_and_color="\x1b[${_di_color_raw}m${dir_parts[-1]}\x1b[39m/"
+    elif [[ -h "$file" ]]; then
+        _file_and_color="\x1b[${_ln_color_raw}m${dir_parts[-1]}\x1b[39m/"
+    elif [[ -c "$file" ]]; then
+        _file_and_color="\x1b[${_cd_color_raw}m${dir_parts[-1]}\x1b[39m/"
+    elif [[ -b "$file" ]]; then
+        _file_and_color="\x1b[${_bd_color_raw}m${dir_parts[-1]}\x1b[39m/"
+    elif [[ ! -e "$file" ]]; then
+        _file_and_color="\x1b[31;1m${dir_parts[-1]}\x1b[0m\x1b[39m"
+    else
+        _file_and_color="\x1b[31;1mUNKNOWN FILE TYPE\x1b[39m"
+        return 42
+    fi
+
+    printf "${leading_slash}${tmp_dir}${_file_and_color}\x1b[0m"
+}
+
+function _enum_exit_code() {
+    (( ${#@} != 3 )) && print '$#@ != 3, quitting' && return 42
+    local archive="${2}"
+    local destination="${3}"
+    case $1 in
+        0) print -n "Extracted $(_colorizer ${archive}) -> $(_colorizer_abs_path ${destination:a})" ;;
+        1) print -n "Destination folder exists, renaming: $(_colorizer ${archive}) -> $(_colorizer_abs_path ${destination:a})" ;;
+        2) print -n "Destination file exists, renaming: $(_colorizer ${archive}) -> $(_colorizer_abs_path ${destination:a})" ;;
+        3) print -n "Unknown error" ;;
+        4) print -n "Wrong file type: $(_colorizer ${archive})" ;;
+        42) print -n "Could not find $(_colorizer $archive)" ;;
+        43) print -n "$(_colorizer ${destination:a}) already exists" ;;
+        44) print -n "Permission denied to write to dir: $(_colorizer_abs_path ${destination:a})" ;;
+        45) print -n "Destination is a file: $(_colorizer_abs_path ${destination:a})" ;;
+        46) print -n "Wrong number of input args given" ;;
+        47) print -n "No archive given" ;;
+        48) print -n "No destination given" ;;
+    esac
+}
+
+# - - - - - - - - - - - - - - - - - - - -
 # - - - - - - - COMPLETION- - - - - - - -
 # - - - - - - - - - - - - - - - - - - - -
 
